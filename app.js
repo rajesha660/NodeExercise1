0300e45d70135223785f0b59d94d82bd9812a089
rajesha <rajesh.ayyavu@object-frontier.com>
2014-12-19 16:26:54 +0530

    Run it in a cluster mode

--- app.js
+++ app.js
@@ -1,12 +1,41 @@
 var https = require('https')
-	, fs = require('fs');
+	, fs = require('fs')
+	, cluster = require('cluster')
+	, numCPUs = require('os').cpus().length;
 	
 var options = {
 	key: fs.readFileSync(__dirname + '/ssl/nodeexercise1.pem'),
 	cert: fs.readFileSync(__dirname + '/ssl/nodeexercise2.crt')
 };
 
-https.createServer(options, function(req, res) {
-	res.writeHead(200);
-	res.end("Hello world \n");
-}).listen(8080);
+
+if(cluster.isMaster) {
+	for(var i = 0; i < numCPUs; i++) {
+		cluster.fork();
+	}
+	
+	cluster.on('fork', function(worker) {
+		console.log(worker + "Worker is forked");
+	});
+	
+	cluster.on('listening', function(worker, address) {
+		console.log(worker + "is listening on " + address);
+	});
+	
+	cluster.on('online', function(worker) {
+		console.log(worker + "Worker is online");
+	});
+	
+	cluster.on('disconnect', function(worker) {
+		console.log(worker + "Worker is disconnect");
+	});
+	
+	cluster.on('exit', function(worker, code, signal) {
+		console.log('worker ' + worker.process.pid + ' died');
+	});
+} else {
+	https.createServer(options, function(req, res) {
+		res.writeHead(200);
+		res.end("Hello world \n");
+	}).listen(8080);
+}
