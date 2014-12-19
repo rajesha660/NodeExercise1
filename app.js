ad18ab516f522a5c925810828266e78341fcdd50
rajesha <rajesh.ayyavu@object-frontier.com>
2014-12-19 20:02:10 +0530

    Implement Domains for error handling

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 var https = require('https')
 	, fs = require('fs')
 	, cluster = require('cluster')
-	, numCPUs = require('os').cpus().length;
+	, numCPUs = require('os').cpus().length
+	, domain = require('domain')
+	, express = require('express')
+	, app = express()
+	, cons = require('consolidate');
 	
 var options = {
 	key: fs.readFileSync(__dirname + '/ssl/nodeexercise1.pem'),
 	cert: fs.readFileSync(__dirname + '/ssl/nodeexercise2.crt')
 };
 
+app.engine('html', cons.swig);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
 
 if(cluster.isMaster) {
 	for(var i = 0; i < numCPUs; i++) {
@@ -28,14 +35,48 @@ if(cluster.isMaster) {
 	
 	cluster.on('disconnect', function(worker) {
 		console.log(worker + "Worker is disconnect");
+		cluster.fork();
 	});
 	
 	cluster.on('exit', function(worker, code, signal) {
 		console.log('worker ' + worker.process.pid + ' died');
 	});
 } else {
-	https.createServer(options, function(req, res) {
-		res.writeHead(200);
-		res.end("Hello world \n");
-	}).listen(8080);
+
+	var server = https.createServer(options, app);
+	
+	app.use(function(req, res, next) {
+	
+		var d = domain.create();
+		d.on('error', function(err) {
+			
+			console.error('error', err.stack);
+			try {
+				
+				var killerTimer = setTimeout(function() {
+					process.exit(1);
+				}, 30000);
+				killtimer.unref();
+				server.close();
+				cluster.worker.disconnect();
+				res.statusCode = 500;
+				res.setHeader('content-type', 'text/plain');
+				res.end('Oops, there was a problem!\n');
+			} catch(er2) {
+				console.error('Error sending 500!', er2.stack);
+			}	
+		});
+	
+		d.add(req);
+		d.add(res);
+		
+		next();
+	});
+	
+	server.listen(8080);
 }
+
+app.get('/', function(req, res) {
+	res.render('home');
+});
+
